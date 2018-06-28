ebc5db25772781519ccd46e171127aecb8f73970
simonpedrobolivar <s.pedro@gmx.de>
2018-06-28 13:43:34 +0200

    Extend functionality of file_coverage_runit Now the function also works if the test functions within the test files are not explicitly called within the test files. This is achieved by loading all functions from the test files into a temporary environment and then append the function calls (e.g. test.sum()) to the respective test files

--- R/file_coverage_runit.R
+++ R/file_coverage_runit.R
@@ -1,28 +1,54 @@
-#' Extends the covr::file_coverage function to also accept test files based on RUnit
+#' Extends the [covr::file_coverage()] function to also accept test files based on RUnit
+#' @description So far only works if the test-functions from the \code{test_files} are explicitally called within the same file.
 #' @param source_files Character vector of pathes to source files with function definitions to measure coverage
 #' @param test_files Character vector of pathes to test files with code to test the functions
 #' @param package_name Character with the name of the package the \code{source_files} and \code{test_files} come from
-#' @param ... Additional arguments passed to \code{\link[covr]{package_coverage}}
-#' @return asdf
+#' @param ... Additional arguments passed to [covr::package_coverage()]
+#' @return an covr object (identital to [covr::package_coverage()])
 #' @export
 
 file_coverage_runit <- function(source_files,
                                 test_files,
+                                #run_test,
                                 package_name,
                                 ...){
 
-  temp_dir <- tempfile() #"C:/User/temp" #tempfile()
+  #source_files <- src
+  #test_files <- test
+  #package_name <- "testpackage3"
+
+  temp_dir <- tempfile()
   #print(temp_dir)
 
   dir.create(file.path(temp_dir))
   # create temporary package with the same name as the package the test and source files come from
   suppressMessages(devtools::create(file.path(temp_dir, package_name), quiet = T))
   dir.create(file.path(temp_dir, package_name, "tests"))
-  # copy the files to new package
+  dir.create(file.path(temp_dir, package_name, "inst"))
+  #file.copy(from = run_test, to = file.path(temp_dir, package_name, "test"))
+
+  env <- new.env()
+  testfuns <- list()
   for(i in 1:length(source_files)){
+    # copy the files to new package
+    file.copy(from = test_files[i], to = file.path(temp_dir, package_name,"tests"))
     file.copy(from = source_files[i], to = file.path(temp_dir, package_name,"R"))
-    file.copy(from = test_files[i], to = file.path(temp_dir, package_name, "tests"))
+    # load test file
+    source(test_files[i], local = env)
+    # read all objects from test file into temporary environment
+    testfuns <- ls(envir = env)#[c(8,9)]
+    rm(list = ls(envir = env), envir = env)
+    for(j in 1:length(testfuns)){
+      testfuns[j] <- paste0(testfuns[j], "()")
+    }
+    # append function call to test files (e.g. test.sum())
+    write(testfuns,
+          file.path(temp_dir, package_name,"tests",
+                    basename(test_files[i])),
+          append = T)
   }
+
+  devtools::load_all(file.path(temp_dir, package_name))
   # check coverage of the new package
   cov <- covr::package_coverage(file.path(temp_dir, package_name))
   attr(cov, "package")$package <- ""
